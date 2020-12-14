9174d56865b364711ea6a987d0d614f35f07c33b
Andreea Cojocaru <andra@Andreeas-MacBook-Pro.local>
2020-12-14 23:46:26 +0200

    finally finished level11

--- level11.py
+++ level11.py
@@ -29,6 +29,30 @@ def get_sqaure(arr,row,col):
             around.append(arr[crow][ccol])
     return around
 
+def get_viewed_seats_status(arr,origx,origy,xstep,ystep):
+    x = origx+xstep
+    y = origy+ystep
+    xlen = len(arr)
+    ylen = len(arr[origx])
+    while x >= 0 and y >=0 and x < xlen and y < ylen:
+        if arr[x][y] != '.':
+            return arr[x][y]
+        x+= xstep
+        y+= ystep
+
+    return None
+
+def get_sqaure2(arr,row,col):
+    around = []
+    around.append(get_viewed_seats_status(arr,row,col, -1, -1))
+    around.append(get_viewed_seats_status(arr,row,col, -1, 0))
+    around.append(get_viewed_seats_status(arr,row,col, -1, 1))
+    around.append(get_viewed_seats_status(arr,row,col, 0, 1))
+    around.append(get_viewed_seats_status(arr,row,col, 1, 1))
+    around.append(get_viewed_seats_status(arr,row,col, 1, 0))
+    around.append(get_viewed_seats_status(arr,row,col, 1, -1))
+    around.append(get_viewed_seats_status(arr,row,col, 0, -1))
+    return around
 
 def turn(arr):
     nextphase = []
@@ -55,16 +79,41 @@ def turn(arr):
                 changes +=1
     return nextphase,changes
 
+def turn2(arr):
+    nextphase = []
+    changes = 0
+    for row in range(len(arr)):
+        nextphase.append([None] * len(arr[row]))
+        for col in range(len(arr[row])):
+            if arr[row][col] == '.':
+                nextphase[row][col] = '.'
+                continue
+            around = get_sqaure2(arr,row,col)
+            if arr[row][col] == '#':
+                if around.count('#') >= 5:
+                    nextphase[row][col] = 'L'
+                else:
+                    nextphase[row][col] = '#'
+            if arr[row][col] == 'L':
+                if around.count('#') == 0:
+                    nextphase[row][col] = '#'
+                else:
+                    nextphase[row][col] = 'L'
+            if arr[row][col] != nextphase[row][col]:
+                # print(arr[row][col],nextphase[row][col])
+                changes +=1
+    return nextphase,changes
+
 def main():
     inputpath = 'input/inputlevel11'
     # inputpath = 'input/demoinput'
     inputarray = read_input(inputpath,parse_input)
     # print(inputarray)
-    arr,changes = turn(inputarray)
+    arr,changes = turn2(inputarray)
     count = 0
     while changes != 0:
         count +=1
-        arr,changes = turn(arr)
+        arr,changes = turn2(arr)
     print(count)
     occupied = 0
     for i in arr:
